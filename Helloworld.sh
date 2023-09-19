be41acb45ad29cf0207d7b4f923b7e4fd1dbbdc2
Sabarish <Sabarish.m417@gmail.com>
2023-09-19 15:03:57 +0530

    updated echo statement

--- Helloworld.sh
+++ Helloworld.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 echo "Hello world, I am learning Devops with AWS"
+
+echo "Ramesh: Hi Surehs, What's up?"
+echo "Suresh: Good morining Ramesh"
+echo "Ramesh: How are you Suresh"
+echo "Suresh: I am good, How are you Ramesh"
