46e6c9b478efda268ad25083d95b87815544967e
Sabarish <Sabarish.m417@gmail.com>
2023-09-19 15:24:03 +0530

    updated echo statement

---
+++ 03.variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Hello world, I am learning Devops with AWS"
+PERSON1=$1 # this is the first value from the command line
+PERSON2=$2 # this is the second value from the command line
+
+
+echo "$PERSON1: Hi $PERSON2, What's up?"
+echo "$PERSON2: Good morining $PERSON1"
+echo "$PERSON1: How are you $PERSON2"
+echo "$PERSON2: I am good, How are you $PERSON1"
