c205a065017c32213bf6946272891bbad035784a
Sabarish <Sabarish.m417@gmail.com>
2023-09-19 22:18:15 +0530

    Added rule for addition

---
+++ 07.conditions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#check if the number is greater than 10 or not
+
+NUMBER=$1
+
+if [ $NUMBER -GT 10 ]
+then 
+    echo"$NUMBER is greater than 10"
+else
+    echo "$NUMBER IS NOT GREATER THAN 10
+fi
