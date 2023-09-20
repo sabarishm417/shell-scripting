8e574c30f8c3801a17b7d108978efa83a89052e9
Sabarish <Sabarish.m417@gmail.com>
2023-09-21 04:37:49 +0530

    Added rule for addition

---
+++ 15.compare.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Compare two numbers
+
+echo "Enter a Value: "
+read a
+echo "Enter b Value: "
+read b
+
+if [ $a == $b ]
+then
+echo " a is equal to b "
+else
+echo " a is not equals to b "
+fi
