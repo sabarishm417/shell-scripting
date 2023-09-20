4ad8e0d010463676759251393ca3d29a233efd96
Sabarish <Sabarish.m417@gmail.com>
2023-09-20 10:57:50 +0530

    Added rule for addition

--- 10.functions.sh
+++ 10.functions.sh
@@ -9,10 +9,10 @@ VALIDATE(){
     #$1 --> it will receive the argument
     if [ $1 -ne 0 ]
     then 
-        echo "$2 .. FAILURE"
+        echo "$2.. FAILURE"
         exit 1
     else 
-        echo "$2   ...  SUCCESS"
+        echo "$2...  SUCCESS"
     fi
 }
 if [ $USERID -ne 0 ]
