6f796ed8534489b0483c82e6bf4d03807de99088
Sabarish <Sabarish.m417@gmail.com>
2023-09-21 09:50:17 +0530

    Added rule for addition

--- 13.loops.sh
+++ 13.loops.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+DATE=$(date %+F)
+LOGSDIR=/shell-scripting/shellscript-logs
 
 USERID=$(id -u)
 R="\e[31m"
@@ -9,6 +11,18 @@ then
     echo -e "$R ERROR: Please run this script with root access $N"
     exit 1
 fi
+
+#all args are in $@
+for i in $@
+do
+    yum list installed $i 
+    if [ $? -ne 0 ]
+    then 
+        echo "$i is not installed, let's install it"
+        yum install $i -y
+    else    
+        echo -e "$Y $i is already installed $N"
+        fi
 
 
 
