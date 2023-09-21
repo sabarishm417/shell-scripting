86c604399accee3c8532046e707064fadb1fad8d
Sabarish <Sabarish.m417@gmail.com>
2023-09-21 09:35:54 +0530

    Added rule for addition

--- 13.loops.sh
+++ 13.loops.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-# all args are in $@
-for i in $@
-do
-    yum install $i -y
-done
-    
+USERID=$(id -u)
+R="\e[31m
+N="\e[0m"
+
+if [ $USERID -ne 0 ]
+then 
+    echo -e "$R ERROR: Please run this script with root access $N"
+    exit 1
+fi
+
+
